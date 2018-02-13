6846a91b50a1ca729199e247fed0976c25a787e0
Sean Matthews <sematthews@PTW.local>
2018-02-12 21:35:46 -0700

    set initial routes

--- src/router.js
+++ src/router.js
@@ -25,7 +25,9 @@ export default new VueRouter({
   scrollBehavior: () => ({ y: 0 }),
 
   routes: [
-    { path: '/', component: load('Hello') },
+    { path: '/', component: load('tfp') }, // Default
+    { path: '/tfp', component: load('tfp') },
+    // { path: '/daystart', component: load('DayStart') }, // Start the Day
 
     // Always leave this last one
     { path: '*', component: load('Error404') } // Not found
